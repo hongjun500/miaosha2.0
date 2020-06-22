13558bf5b2d522ca760e75f8f4a721d36deb4a37
hongjun500 <bhj.hong@qq.com>
2020-06-22 21:55:31 +0800

    商品模型创建

---
+++ src/main/java/com/hongjun/service/model/ItemModel.java
@@ -0,0 +1,87 @@
+package com.hongjun.service.model;
+
+import java.math.BigDecimal;
+
+/**
+ * @author hongjun500
+ * @date 2020/6/22 20:50
+ * Created with 2019.3.2.IntelliJ IDEA
+ * Description:
+ */
+public class ItemModel {
+    private Integer id;
+
+    /**商品名*/
+    private String title;
+
+    /**价格*/
+    private BigDecimal price;
+
+    /**商品库存*/
+    private Integer stock;
+
+    /**商品描述信息*/
+    private String description;
+
+    /**商品销量*/
+    private Integer sales;
+
+    /**商品图片*/
+    private String imgUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
